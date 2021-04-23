b3ec31ca6ab1e4eb81c27c63d28348481fd097b9
akhil <akhilkhubchandani@gmail.com>
2021-04-23 16:53:52 +0530

    updated email

--- login/index.php
+++ login/index.php
@@ -1,15 +1,16 @@
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <title>Where's My Money</title>
   <link rel="icon" type="image/png" href="wallet.png" sizes="32x32" />
-    <link rel="stylesheet" href="styles_2.css">
+  <link rel="stylesheet" href="styles_2.css">
 
 </head>
+
 <body>
-  
+
   <header>
     <a href="#" class="brand">
       <img src="wallet.png" alt="logo">
@@ -17,7 +18,9 @@
       </div>
     </a>
     <nav>
-      <a href="header.php"><H2>GET STARTED</H2></a>
+      <a href="header.php">
+        <H2>GET STARTED</H2>
+      </a>
     </nav>
   </header>
 
@@ -25,7 +28,9 @@
 
     <div id="content" class="content">
       <h1>WHERE'S MY MONEY?</h1>
-         <h3> < An Expense Manager ></h3>
+      <h3>
+        < An Expense Manager>
+      </h3>
     </div>
 
   </section>
@@ -33,32 +38,32 @@
 
     <div class="event">
       <div class="card" style="background-image:linear-gradient(111.49deg, #57836d 43.29%,#76b88f 96.29%), url(rain_money.png);;">
-        
+
         <h2>ABOUT</h2>
-        
+
         <div class="description">
           <ul>
-               <h3>In daily life, there are a number of transactions that we make and we easily lose track of them due to our busy schedule and fast-moving lives. 
-                This in turn results in mismanagement of our money.
-                But we at Where’s My Money, help you manage your expenses and track your financial activity efficiently.
-                All you need to do is update your activity every time you spend or earn money.   
-                Set budgets and track monthly spendings accordingly. </
-            </ul>  
-               
+            <h3>In daily life, there are a number of transactions that we make and we easily lose track of them due to our busy schedule and fast-moving lives.
+              This in turn results in mismanagement of our money.
+              But we at Where’s My Money, help you manage your expenses and track your financial activity efficiently.
+              All you need to do is update your activity every time you spend or earn money.
+              Set budgets and track monthly spendings accordingly. </ </ul>
+
         </div>
 
-        
+
       </div>
     </div>
 
   </section>
 
   <section class="container contact" id="contact">
-    
-    <a href="mailto:reshmaproy@gmail.com" target="_top" class="btn">
-    <h2>Contact Us</h2>
+
+    <a href="mailto:akhilkhubchandani@gmail.com" target="_top" class="btn">
+      <h2>Contact Us</h2>
     </a>
   </section>
-  
+
 </body>
+
 </html>
